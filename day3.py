0f44b65ce2d9037d6241ecfc071435d0badccaf4
vonnoda <54062227+vonnoda@users.noreply.github.com>
2019-08-21 11:10:28 +0800

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+import numpy as np
+#转置矩阵
+x = np.array([[1,2],[3,4]])
+print(x)
+'''[[1 2]
+    [3 4]]'''
+print(x.T)  #数组对象的T属性是用来转置矩阵
+'''[[1 3]
+    [2 4]]'''
